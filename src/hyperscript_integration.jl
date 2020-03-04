6d7e5a32739deaa6a4abb9a298fe6e22832e8945
lukasrosario <lukasrosario52699@gmail.com>
2020-03-04 16:42:42 -0500

    new tags

--- src/hyperscript_integration.jl
+++ src/hyperscript_integration.jl
@@ -26,7 +26,14 @@ function m_unesc(tag, args...; kw...)
     m(Hyperscript.NOESCAPE_HTMLSVG_CONTEXT, tag, args..., dataJscallId = get_unique_dom_id(); kw...)
 end
 
-for node in (:p, :div, :input, :font, :nav, :ul, :li, :img, :a)
+for node in (:a, :abbr, :address, :area, :article, :aside, :audio, :b,
+    :base, :bdi, :bdo, :blockquote, :body, :br, :button, :canvas, :caption, :cite, :code, :col, :colgroup, :data, :datalist,
+    :dd, :del, :details, :dfn, :dialog, :div, :dl, :dt, :em, :embed, :fieldset, :figcaption, :figure, :footer, :form, :h1, :h2,
+    :h3, :h4, :h5, :h6, :head, :header, :hgroup, :hr, :html, :i, :iframe, :img, :input, :ins, :kbd, :label, :legend, :li, :link,
+     :main, :map, :mark, :math, :menu, :menuitem, :meta, :meter, :nav, :noscript, :object, :ol, :optgroup, :option, :output, :p, :param,
+      :picture, :pre, :progress, :q, :rb, :rp, :rt, :rtc, :ruby, :s, :samp, :script, :section, :select, :slot, :small, :source, :span,
+      :strong, :style, :sub, :summary, :sup, :svg, :table, :tbody, :td, :template, :textarea, :tfoot, :th, :thead, :time, :title, :tr,
+      :track, :u, :ul, :var, :video, :wbr)
     node_name = string(node)
     unesc = Symbol(node_name * "_unesc")
     @eval $(node)(args...; kw...) = um($(node_name), args...; kw...)
