8cffecf046d3e3d8a443e1aba45c9bda950436c1
SimonDanisch <sdanisch@gmail.com>
2021-01-11 10:58:20 +0100

    remove complicated iframe setup

--- src/display.jl
+++ src/display.jl
@@ -172,14 +172,7 @@ function show_in_iframe(server, session, app)
     # to the parent iframe, for which we register an
     # event handler via resize_iframe_parent, which then
     # resizes the parent iframe accordingly
-    route!(server, session_route) do context
-        server = context.application
-        request = context.request
-        insert_session!(server, session)
-        on_document_load(session, js"JSServe.resize_iframe_parent($(session.id))")
-        html_dom = Base.invokelatest(app.handler, session, request)
-        return html(page_html(session, html_dom))
-    end
+    route!(server, session_route => app)
     return jsrender(session, iframe_html(server, session, session_route))
 end
 
@@ -199,33 +192,8 @@ function iframe_html(server::Server, session::Session, route::String)
     # Display the route we just added in an iframe inline:
     url = online_url(server, route)
     remote_origin = online_url(server, "")
-    style = "position: relative; display: block; width: 100%; height: 100%; padding: 0; overflow: hidden; border: none"
-    return DOM.div(
-        js"""
-            function register_resize_handler(remote_origin) {
-                function resize_callback(event) {
-                    if (event.origin !== remote_origin) {
-                        return;
-                    }
-                    const uuid = event.data[0];
-                    const width = event.data[1];
-                    const height = event.data[2];
-                    const iframe = document.getElementById(uuid);
-                    if (iframe) {
-                        iframe.style.width = width + "px";
-                        iframe.style.height = height + "px";
-                    }
-                }
-                if (window.addEventListener) {
-                    window.addEventListener("message", resize_callback, false);
-                } else if (window.attachEvent) {
-                    window.attachEvent("onmessage", resize_callback);
-                }
-            }
-            register_resize_handler($(repr(remote_origin)))
-        """,
-        DOM.iframe(src=url, id=session.id, style=style, scrolling="no")
-    )
+    style = "position: absolute; width: 100%; height: 100%; padding: 0; overflow: hidden; border: none"
+    return DOM.iframe(src=url, id=session.id, style=style, scrolling="no")
 end
 
 function node_html(session::Session, node::Hyperscript.Node)
