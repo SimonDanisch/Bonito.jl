53da7260efc90623166d7b2b915649203a4dbce7
Daniel Lakeland <dlakelan@street-artists.org>
2021-08-31 10:53:50 -0700

    remove reliance on 127.0.0.1 and other ipv4 addresses, make it work with names so that IPv6 can work as well

--- src/server.jl
+++ src/server.jl
@@ -101,7 +101,7 @@ end
 
 function websocket_request()
     headers = [
-        "Host" => "127.0.0.1",
+        "Host" => "localhost",
         "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0",
         "Accept" => "*/*",
         "Accept-Encoding" => "gzip, deflate, br",
@@ -292,7 +292,7 @@ end
 
 function start(application::Server; verbose=false)
     isrunning(application) && return
-    address = Sockets.InetAddr(parse(Sockets.IPAddr, application.url), application.port)
+    address = Sockets.InetAddr(Sockets.getaddrinfo(application.url), application.port)
     ioserver = Sockets.listen(address)
     application.server_connection[] = ioserver
     # pass tcp connection to listen, so that we can close the server
