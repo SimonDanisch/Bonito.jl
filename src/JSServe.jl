8457231b7b68dc09e4262ea581b554088458960a
piever <pietro.vertechi@protonmail.com>
2022-01-13 21:21:57 +0100

    update Asset type

--- src/JSServe.jl
+++ src/JSServe.jl
@@ -19,7 +19,7 @@ using Tables
 using Colors
 using LinearAlgebra
 using CodecZlib
-using RelocatableFolders: @path
+using RelocatableFolders: @path, Path
 
 include("types.jl")
 include("server.jl")
