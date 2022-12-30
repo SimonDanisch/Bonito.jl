754b8cc450273a2263db2c9f89b26dc852f7723b
SimonDanisch <sdanisch@protonmail.com>
2022-12-30 15:02:24 +0100

    update docs + clean up

--- src/display.jl
+++ src/display.jl
@@ -2,17 +2,38 @@ Base.showable(::Union{MIME"text/html", MIME"application/prs.juno.plotpane+html"}
 
 const CURRENT_SESSION = Ref{Union{Nothing, Session}}(nothing)
 
-function Page(; offline=false, exportable=true)
+"""
+    Page(;
+        exportable=true,
+        offline=false,
+        server_config...
+    )
+
+A Page can be used for resetting the JSServe state in a multi page display outputs, like it's the case for Pluto/IJulia/Documenter.
+For Documenter, the page needs to be set to `exportable=true, offline=true`, but doesn't need to, since Page defaults to the most common parameters for known Packages.
+Exportable has the effect of inlining all data & js dependencies, so that everything can be loaded in a single HTML object.
+`offline=true` will make the Page not even try to connect to a running Julia
+process, which makes sense for the kind of static export we do in Documenter.
+For convenience, one can also pass additional server configurations, which will directly
+get put into `configure_server!(;server_config...)`.
+Have a look at the docs for `configure_server!` to see the parameters.
+"""
+function Page(; offline=false, exportable=true, server_config...)
     old_session = CURRENT_SESSION[]
+    HTTPServer.configure_server!(; server_config...)
     if !isnothing(old_session)
         close(old_session)
     end
     CURRENT_SESSION[] = nothing
     if offline
         force_connection!(NoConnection())
+    else
+        force_connection!()
     end
     if exportable
         force_asset_server!(NoServer())
+    else
+        force_asset_server!()
     end
     force_subsession!(true)
     return
