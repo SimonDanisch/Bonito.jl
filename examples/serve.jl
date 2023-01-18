f7857859958468bdfbd7b43af15dd2abc9d4e200
Stefan Krastanov <stefan@krastanov.org>
2023-01-18 10:52:24 -0500

    mention `proxy_url` in the serve.jl example (#148)

--- examples/serve.jl
+++ examples/serve.jl
@@ -23,6 +23,8 @@ if isdefined(Main, :server)
 end
 
 server = JSServe.Server(app, "127.0.0.1", 8081)
+# Important Note: You might want to set the keyword argument `proxy_url` above in case
+# you have a reverse proxy (like nginx or caddy) in front of the JSServe instance.
 JSServe.HTTPServer.start(server)
 # JSServe.HTTPServer.route!(server, "/" => app) # Overwrite app after changing it
 wait(server)
