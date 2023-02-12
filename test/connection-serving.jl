bccff098ecaa5512dd0b0af0392109d0a28fa184
SimonDanisch <sdanisch@protonmail.com>
2023-02-12 18:47:01 +0100

    see where it hangs

--- test/connection-serving.jl
+++ test/connection-serving.jl
@@ -13,18 +13,18 @@ function export_test_app(session, request)
     )
 end
 
-# connections = [(:NoConnection, ()-> NoConnection()), (:WebSocketConnection, ()-> WebSocketConnection())]
-# servers = [(:NoServer, () -> NoServer()), (:HTTPAssetServer, () -> HTTPAssetServer())]
+connections = [(:NoConnection, ()-> NoConnection()), (:WebSocketConnection, ()-> WebSocketConnection())]
+servers = [(:NoServer, () -> NoServer()), (:HTTPAssetServer, () -> HTTPAssetServer())]
 
-# path = joinpath(@__DIR__, "test.html")
-# @testset "connection $(c) server: $(s)" for ((c, connection), (s, server)) in Iterators.product(connections, servers)
-#     app = App(export_test_app)
-#     export_static(path, app; connection=connection(), asset_server=server())
-#     # We need to drop a bit lower and cant use `testapp` here, since that uses fixed connection + asset server
-#     window = Window(URI(path))
-#     result = run(window, "$(query_testid("result")).innerText")
-#     @test result == "passed"
-#     close(window)
-#     close(app)
-# end
-# rm(path; force=true)
+path = joinpath(@__DIR__, "test.html")
+@testset "connection $(c) server: $(s)" for ((c, connection), (s, server)) in Iterators.product(connections, servers)
+    app = App(export_test_app)
+    export_static(path, app; connection=connection(), asset_server=server())
+    # We need to drop a bit lower and cant use `testapp` here, since that uses fixed connection + asset server
+    window = Window(URI(path))
+    # result = run(window, "$(query_testid("result")).innerText")
+    # @test result == "passed"
+    close(window)
+    close(app)
+end
+rm(path; force=true)
