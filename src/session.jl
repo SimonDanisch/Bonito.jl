71f8307927d7e9045bcba17e1e7454f52841fce8
SimonDanisch <sdanisch@protonmail.com>
2023-03-27 17:27:58 +0200

    locking task switches, so can't be used in finalizer

--- src/session.jl
+++ src/session.jl
@@ -74,31 +74,29 @@ function get_session(session::Session, id::String)
 end
 
 function free(session::Session)
-    return lock(root_session(session).deletion_lock) do
-        session.status === CLOSED && return
-        # unregister all cached objects from root session
-        root = root_session(session)
-        # If we're a child session, we need to remove all objects trackt in our root session:
-        if session !== root
-            # We need to remove our session from the parent first, otherwise `delete_cached!`
-            # will think our session still holds the value, which would prevent it from deleting
-            delete!(parent(session).children, session.id)
-            for key in keys(session.session_objects)
-                if haskey(root.session_objects, key)
-                    delete_cached!(root, key)
-                end
+    session.status === CLOSED && return
+    # unregister all cached objects from root session
+    root = root_session(session)
+    # If we're a child session, we need to remove all objects trackt in our root session:
+    if session !== root
+        # We need to remove our session from the parent first, otherwise `delete_cached!`
+        # will think our session still holds the value, which would prevent it from deleting
+        delete!(parent(session).children, session.id)
+        for key in keys(session.session_objects)
+            if haskey(root.session_objects, key)
+                delete_cached!(root, key)
             end
         end
-        # delete_cached! only deletes in the root session so we need to still empty the session_objects:
-        empty!(session.session_objects)
-        empty!(session.on_document_load)
-        empty!(session.message_queue)
-        # remove all listeners that where created for this session
-        foreach(off, session.deregister_callbacks)
-        empty!(session.deregister_callbacks)
-        session.status = CLOSED
-        return
     end
+    # delete_cached! only deletes in the root session so we need to still empty the session_objects:
+    empty!(session.session_objects)
+    empty!(session.on_document_load)
+    empty!(session.message_queue)
+    # remove all listeners that where created for this session
+    foreach(off, session.deregister_callbacks)
+    empty!(session.deregister_callbacks)
+    session.status = CLOSED
+    return
 end
 
 function Base.close(session::Session)
