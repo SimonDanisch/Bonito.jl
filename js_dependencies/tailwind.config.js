79e4651a4d67afdcaf827a34aa23d2365f50e319
SimonDanisch <sdanisch@protonmail.com>
2023-05-25 17:38:49 +0200

    remove slow close + minify tailwind

---
+++ js_dependencies/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./tailwind-purge.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
