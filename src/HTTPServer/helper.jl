05029a2102b5ee0f569c594822852d5040ef773a
Elliot Saba <staticfloat@gmail.com>
2023-08-09 09:33:16 -0700

    Provide CORS header for 404 errors
    This just makes debugging in VSCode a bit nicer.

--- src/HTTPServer/helper.jl
+++ src/HTTPServer/helper.jl
@@ -3,5 +3,11 @@ function html(body)
 end
 
 function response_404(body="Not Found")
-    return HTTP.Response(404, ["Content-Type" => "text/html", "charset" => "utf-8"], body=body)
+    return HTTP.Response(404, [
+        "Content-Type" => "text/html",
+        "charset" => "utf-8",
+        # Avoids throwing lots of errors in the devtools console when
+        # VSCode tries to load non-existent resources from the plots pane.
+        "Access-Control-Allow-Origin" => "*",
+    ], body=body)
 end
