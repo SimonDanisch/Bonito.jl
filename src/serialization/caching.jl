50c55a838d810dddd7831da25d6e93aed279546e
SimonDanisch <sdanisch@protonmail.com>
2023-10-12 14:34:30 +0200

    correctly fix deregister callbacks for OnUpdate

--- src/serialization/caching.jl
+++ src/serialization/caching.jl
@@ -26,8 +26,9 @@ function register_observable!(session::Session, obs::Observable)
     # Only register one time
     if !haskey(root.session_objects, obs.id)
         updater = JSUpdateObservable(root, obs.id)
-        on(updater, root, obs)
-        # push!(session.deregister_callbacks, deregister)
+        # Don't deregister on root / or session close
+        # The updaters callbacks are freed manually in delete_cached!`
+        on(updater, obs)
     end
     return
 end
@@ -93,7 +94,8 @@ function add_cached!(create_cached_object::Function, session::Session, send_to_j
     haskey(session.session_objects, key) && return result
     # Now, we have two code paths, depending on whether we have a child session or a root session
     root = root_session(session)
-    if root === session # we are root, so we simply cache the object (we already checked it's not cached yet)
+    # we are root, so we simply cache the object (we already checked it's not cached yet)
+    if root === session
         send_to_js[key] = create_cached_object()
         session.session_objects[key] = object
         return result
