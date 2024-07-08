3243e63306f1aba2baba32130e0c02cde7368c98
Simon <sdanisch@protonmail.com>
2024-07-08 15:51:42 +0200

    maybe fix example

--- src/interactive.jl
+++ src/interactive.jl
@@ -34,7 +34,7 @@ routes, task, server = interactive_server(Website.asset_paths()) do
 end
 
 # Once everything looks good, export the static site
-dir = @path joinpath(@__DIR__, "docs")
+dir = joinpath(@__DIR__, "docs")
 # only delete the bonito generated files
 rm(joinpath(dir, "bonito"); recursive=true, force=true)
 Bonito.export_static(dir, routes)
