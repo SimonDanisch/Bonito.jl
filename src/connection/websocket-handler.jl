c793c6c80aa165be5694cf6c537bc8056b3cbb0e
Frankie Robertson <frankie@robertson.name>
2024-08-12 11:57:14 +0300

    Add `WebSocketHandler` helper for websocket-based FrontendConnection extensions

---
+++ src/connection/websocket-handler.jl
@@ -0,0 +1,113 @@
+using HTTP.WebSockets: WebSocket, WebSocketError
+using HTTP.WebSockets: receive, isclosed
+using HTTP.WebSockets
+
+struct WebSocketHandler
+    socket::WebSocket
+    lock::ReentrantLock
+end
+
+WebSocketHandler(socket) = WebSocketHandler(socket, ReentrantLock())
+
+function safe_read(websocket)
+    try
+        # readavailable is what HTTP overloaded for websockets
+        return receive(websocket)
+    catch e
+        if WebSockets.isok(e)
+            # it's ok :shrug:
+        elseif e isa Union{Base.IOError, EOFError}
+            @info("WS connection closed because of IO error")
+            return nothing
+        else
+            rethrow(e)
+        end
+    end
+end
+
+function safe_write(websocket, binary)
+    try
+        # send is what HTTP overloaded for writing to a websocket
+        send(websocket, binary)
+        return true
+    catch e
+        if WebSockets.isok(e) || e isa Union{Base.IOError,EOFError}
+            @warn "sending message to a closed websocket" maxlog = 1
+            # it's ok :shrug:
+            return nothing
+        else
+            rethrow(e)
+        end
+    end
+end
+
+function Base.isopen(ws::WebSocketHandler)
+    isnothing(ws.socket) && return false
+    # isclosed(ws.socket) returns readclosed && writeclosed
+    # but we consider it closed if either is closed?
+    if ws.socket.readclosed || ws.socket.writeclosed
+        return false
+    end
+    # So, it turns out, ws connection where the tab gets closed
+    # stay open indefinitely, but aren't writable anymore
+    # TODO, figure out how to check for that
+    return true
+end
+
+function Base.write(ws::WebSocketHandler, binary)
+    if isnothing(ws.socket)
+        error("socket closed or not opened yet")
+    end
+    lock(ws.lock) do
+        written = safe_write(ws.socket, binary)
+        if written != true
+            @debug "couldnt write, closing ws"
+            close(ws)
+        end
+    end
+end
+
+function Base.close(ws::WebSocketHandler)
+    isnothing(ws.socket) && return
+    try
+        socket = ws.socket
+        ws.socket = nothing
+        isclosed(socket) || close(socket)
+    catch e
+        if !WebSockets.isok(e)
+            @warn "error while closing websocket" exception=(e, Base.catch_backtrace())
+        end
+    end
+end
+
+
+"""
+    runs the main connection loop for the websocket
+"""
+function run_connection_loop(session::Session, handler::WebSocketHandler)
+    @debug("opening ws connection for session: $(session.id)")
+    websocket = handler.socket
+    while !isclosed(websocket)
+        bytes = safe_read(websocket)
+        # nothing means the browser closed the connection so we're done
+        isnothing(bytes) && break
+        try
+            process_message(session, bytes)
+        catch e
+            # Only print any internal error to not close the connection
+            @warn "error while processing received msg" exception = (e, Base.catch_backtrace())
+        end
+    end
+end
+
+
+"""
+    returns the javascript snippet to setup the connection
+"""
+function setup_websocket_connection_js(proxy_url, session::Session)
+    return js"""
+        $(Websocket).then(WS => {
+            WS.setup_connection({proxy_url: $(proxy_url), session_id: $(session.id), compression_enabled: $(session.compression_enabled)})
+        })
+    """
+end
