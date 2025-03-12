03a9082f2c6970c329023a193eaf17b9413a32b1
SimonDanisch <sdanisch@protonmail.com>
2025-03-12 19:24:22 +0100

    lock, so binary fetches are in correct order

--- src/js_source.jl
+++ src/js_source.jl
@@ -164,12 +164,14 @@ function inline_code(session::Session, asset_server, js::JSCode)
         interpolated_objects = Dict(v => k for (k, v) in context.objects)
         binary = BinaryAsset(session, interpolated_objects)
         src = """
-            // JSCode from $(js.file)
+        // JSCode from $(js.file)
+        Bonito.OBJECT_FREEING_LOCK.lock(() => {
             Bonito.fetch_binary('$(url(session, binary))').then(bin_messages=>{
                 const objects = Bonito.decode_binary(bin_messages, $(session.compression_enabled));
                 const __lookup_interpolated = (id) => objects[id]
                 $code
             })
+        })
         """
     end
     return inline_code(session, asset_server, src)
