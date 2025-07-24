74186cfeb53946103d93cd64341fe92a0be3ba91
SimonDanisch <sdanisch@protonmail.com>
2025-07-24 15:20:35 +0200

    fix tests

--- js_dependencies/Bonito.js
+++ js_dependencies/Bonito.js
@@ -31,6 +31,7 @@ const {
     lock_loading,
     OBJECT_FREEING_LOCK,
     free_object,
+    force_free_object,
 } = Sessions;
 
 function update_node_attribute(node, attribute, value) {
@@ -157,6 +158,8 @@ const Bonito = {
     update_dom_node,
     lookup_global_object,
     update_or_replace,
+    force_free_object,
+
     OBJECT_FREEING_LOCK,
     can_send_to_julia,
     onany,
