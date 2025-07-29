0784a72f75a3ce641fca968f3293586ada6a549e
SimonDanisch <sdanisch@protonmail.com>
2025-07-29 15:07:41 +0200

    check for time out value

--- src/app.jl
+++ src/app.jl
@@ -165,7 +165,8 @@ function HTTPServer.apply_handler(handler::DisplayHandler, context)
 end
 
 function wait_for_ready(app::App; timeout=100)
-    wait_for(()-> !isnothing(app.session[]); timeout=timeout)
+    success = wait_for(()-> !isnothing(app.session[]); timeout=timeout)
+    success !== :success && return nothing
     isclosed(app.session[]) && return nothing
     wait_for(timeout=timeout) do
         isready(app.session[]) || isclosed(app.session[])
