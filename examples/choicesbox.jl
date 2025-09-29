b80bae0c045f812d0859ae1c825f418f16a5ff8a
Simon <sdanisch@protonmail.com>
2025-09-29 20:34:15 +0200

    cleanup choices example (#361)

--- examples/choicesbox.jl
+++ examples/choicesbox.jl
@@ -2,33 +2,32 @@ using Bonito
 using Observables
 # Example usage function
 
-# sample options
-const fruits = ["Cherry", "Apple", "Banana", "Date", "Elderberry",  "Fig", "Grape"]
-
-# Configure Choices.js parameters
-const params = ChoicesJSParams(
-    searchPlaceholderValue="Type to search fruits...",
-    itemSelectText= "Press this fruit to select",
-    searchEnabled=true,
-    shouldSort=false,
-    searchResultLimit=3,
-    renderChoiceLimit=10,
-    placeholder = true,
-    placeholderValue = "--"
-)
-const choicesbox = ChoicesBox(fruits; choicejsparams=params)
-
-# Display selected value
-const selected_display = map(choicesbox.value) do value
-    "Selected: $value"
-end
-
-# Handle value changes
-on(choicesbox.value) do value
-    @info "ChoicesBox value changed to: $value"
-end
-
 App() do
+    # sample options
+    fruits = ["Cherry", "Apple", "Banana", "Date", "Elderberry",  "Fig", "Grape"]
+
+    # Configure Choices.js parameters
+    params = ChoicesJSParams(
+        searchPlaceholderValue="Type to search fruits...",
+        itemSelectText= "Press this fruit to select",
+        searchEnabled=true,
+        shouldSort=false,
+        searchResultLimit=3,
+        renderChoiceLimit=10,
+        placeholder = true,
+        placeholderValue = "--"
+    )
+    choicesbox = ChoicesBox(fruits; choicejsparams=params)
+
+    # Display selected value
+    selected_display = map(choicesbox.value) do value
+        "Selected: $value"
+    end
+
+    # Handle value changes
+    on(choicesbox.value) do value
+        @info "ChoicesBox value changed to: $value"
+    end
     return Card(
         DOM.div(
         DOM.h2("ChoicesBox Example"),
@@ -38,5 +37,3 @@ App() do
         )
     )
 end
-
-##
